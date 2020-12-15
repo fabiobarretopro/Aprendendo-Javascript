05a3d933a26877e57ef5fb5e79e24a5dc4ad4938
fabioromaobarreto <romaobarreto@hotmail.com>
2020-12-15 20:45:22 -0300

    Evoluindo em Javascript

---
+++ iife-modal.js
@@ -0,0 +1,8 @@
+(function(){
+            let isValid = true
+            console.log("init menu", isValid)
+            function init(){
+                        console.log("init modal",)
+            }
+            init()
+})()
