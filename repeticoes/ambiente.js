49c843d0237656c82c179be6babb356ffd3e6372
fabioromaobarreto <romaobarreto@hotmail.com>
2021-01-05 18:19:13 -0300

    repetições

---
+++ repeticoes/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+while(c <=6){
+            console.log("Testando!")
+            c = c + 1
+}
+var c = 1
+do{
+            console.log("Testando!")
+            c = c + 1    
+}while(c <=6)
+
