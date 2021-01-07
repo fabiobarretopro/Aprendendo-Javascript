2686f6195813ee53ec645cd33b88c91fd23f9d17
fabiobarreto_pro <fabiobarreto_pro@hotmail.com>
2021-01-06 22:51:53 -0300

    Repetições

--- repeticoes/ambiente.js
+++ repeticoes/ambiente.js
@@ -1,12 +1,13 @@
-/*var c = 1
-while(c <=6){
-            console.log("Testando!")
-            c = c + 1
-}*/
-var c = 1
+let num = [5, 8, 2, 9, 3]
+num[3] = 6
+num.push(1)
+num.sort()
 
-do{
-            console.log("Testando!")
-            c = c + 1    
-}while(c <=6)
 
+console.log(`Nosso vetor é o ${num}`)
+console.log(`Existem ${num.length} elementos`)
+console.log(`O valor 2 está na posição ${num.indexOf(2)}`)
+
+for(let c in num){
+            console.log(num[c])
+}
