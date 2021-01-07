2b9c5f4321d00872e65922c82fea34c6bac7b397
fabiobarreto_pro <fabiobarreto_pro@hotmail.com>
2021-01-06 21:19:10 -0300

    comitando

--- repeticoes/ambiente.js
+++ repeticoes/ambiente.js
@@ -1,9 +1,10 @@
-var c = 1
+/*var c = 1
 while(c <=6){
             console.log("Testando!")
             c = c + 1
-}
+}*/
 var c = 1
+
 do{
             console.log("Testando!")
             c = c + 1    
